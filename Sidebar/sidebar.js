f60e534bdf0d40116d8ee1e3aeb4ea858db9b723
Matthew Chae <mschae94@hotmail.com>
2018-07-10 17:42:19 +0100

    Initial commit

---
+++ Sidebar/sidebar.js
@@ -0,0 +1,8 @@
+$('#sidebarCollapse').on('click', function () {
+    $('#sidebar').addClass('active');
+    $('.overlay').fadeIn();
+});
+$('.overlay').on('click', function () {
+    $('#sidebar').removeClass('active');
+    $('.overlay').fadeOut();
+});
